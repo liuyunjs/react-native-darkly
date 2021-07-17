7984b781f48d33eed856853ae78b12aacc0a27f8
liuyunjs <429695034@qq.com>
2021-07-17 16:10:52 +0800

    dark 改成 forceDark

--- library/darkly.tsx
+++ library/darkly.tsx
@@ -12,18 +12,20 @@ export function darkly<T extends {}, E = {}>(
   const Darkly = React.forwardRef<
     any,
     // @ts-ignore
-    T & { darkStyle?: T['style']; dark?: boolean } & E
+    T & { darkStyle?: T['style']; forceDark?: boolean } & E
   >((props: any, ref) => {
     const darklyProps: any = {};
 
     const colorScheme = useColorScheme();
 
     const isDark =
-      typeof props.dark === 'boolean' ? props.dark : colorScheme === 'dark';
+      typeof props.forceDark === 'boolean'
+        ? props.forceDark
+        : colorScheme === 'dark';
 
     if (isDark) {
       styleKeys.forEach((key) => {
-        const darkKey = `dark${key.charAt(0).toUpperCase()}${key.slice(1)}`;
+        const darkKey = `dark${key[0].toUpperCase()}${key.slice(1)}`;
 
         if (props[key] || props[darkKey]) {
           if (!darklyProps[key]) darklyProps[key] = [];
